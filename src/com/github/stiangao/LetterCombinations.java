e69139048f429dc12e5f1f231df2c92b990b4ba3
tiangao <tiangao1102@163.com>
2018-02-08 00:40:12 +0800

    p17 nice Solution

--- src/com/github/stiangao/LetterCombinations.java
+++ src/com/github/stiangao/LetterCombinations.java
@@ -1,6 +1,7 @@
 package com.github.stiangao;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -10,12 +11,45 @@ public class LetterCombinations {
 
     /**
      * 题目解释： 数字上对应字符串编码，输入数字，输出所有数字对应字母的排列组合结果
+     *
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println(new LetterCombinations().letterCombinations("237"));
+        System.out.println(new LetterCombinations().letterCombinations2("237"));
     }
 
+    /**
+     * 先进先出队列解法，每轮结果依次拼上后一个 chars 的遍历再加入数组
+     *
+     * @param digits
+     * @return
+     */
+    public List<String> letterCombinations2(String digits) {
+        LinkedList<String> ans = new LinkedList<>();
+        if (digits.isEmpty()) {
+            return ans;
+        }
+        String[] mapping = new String[]{"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        ans.add("");
+        for (int i = 0; i < digits.length(); i++) {
+            char[] chars = mapping[(digits.charAt(i) - '0')].toCharArray();
+            int size = ans.size();
+            for (int j = 0; j < size; j++) {
+                for (char ch : chars) {
+                    ans.add(ans.getFirst() + ch);
+                }
+                ans.removeFirst();
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * 朴素解法，计算每个字母在哪个位置，通过划分和取余计算得到结果
+     *
+     * @param digits
+     * @return
+     */
     public List<String> letterCombinations(String digits) {
         String[] numStrings = new String[]{"", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         char[] numChars = digits.toCharArray();
